3299191b85181f16dc5e33bf157af31394529020
blecua84 <josejavier.blecua@gmail.com>
2013-09-07 18:52:37 +0200

    Cambios configuración

--- SNSANGELGUARD/DISTRIBUTION/harvestedSNSAngelGuard/script/harvestedSNSAngelGuardFB.sh
+++ SNSANGELGUARD/DISTRIBUTION/harvestedSNSAngelGuard/script/harvestedSNSAngelGuardFB.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#Programa automático de backups de información para la aplicación SNSAngelGuardFB
+#Actualiza la información de la base de datos y ejecuta los filtros
 
 DATE=$(date +%Y_%m_%d);
 logFile=/usr/local/snsangelguardfb/harvested/logs/execute_harvesting_SNSAngelGuardFB_$DATE\.log;
-cd /usr/local/snsangelguardfb/harvested/harvestedSNSAngelGuard/
+cd /Users/josejavierblecuadepedro1/Documents/angelnet/SNSANGELGUARD/SOURCES/harvestedSNSAngelGuard
 mvn clean dependency:copy-dependencies package >> $logFile
-mvn exec:java -Dexec.mainClass="es.uah.cc.ie.harvestedsnsangelguard.Main HOST PATH_KEYSTORE PASS_KEYSTORE" >> $logFile
+mvn exec:java -Dexec.mainClass="es.uah.cc.ie.harvestedsnsangelguard.Main" -Dexec.args="https://localhost:8443/ /Users/user/.keystore passuser" >> $logFile
--- SNSANGELGUARD/SOURCES/harvestedSNSAngelGuard/script/harvestedSNSAngelGuardFB.sh
+++ SNSANGELGUARD/SOURCES/harvestedSNSAngelGuard/script/harvestedSNSAngelGuardFB.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#Programa automático de backups de información para la aplicación SNSAngelGuardFB
+#Actualiza la información de la base de datos y ejecuta los filtros
 
 DATE=$(date +%Y_%m_%d);
-logFile=/Users/josejavierblecuadepedro1/Documents/angelnet/SNSANGELGUARD/SOURCES/harvestedSNSAngelGuard/execute_harvesting_SNSAngelGuardFB_$DATE\.log;
-cd /Users/josejavierblecuadepedro1/Documents/angelnet/SNSANGELGUARD/SOURCES/harvestedSNSAngelGuard/
+logFile=/usr/local/snsangelguardfb/harvested/logs/execute_harvesting_SNSAngelGuardFB_$DATE\.log;
+cd /Users/josejavierblecuadepedro1/Documents/angelnet/SNSANGELGUARD/SOURCES/harvestedSNSAngelGuard
 mvn clean dependency:copy-dependencies package >> $logFile
-mvn exec:java -Dexec.mainClass="es.uah.cc.ie.harvestedsnsangelguard.Main http://localhost:8080/ /Users/josejavierblecuadepedro1/.keystore blecua" >> $logFile
+mvn exec:java -Dexec.mainClass="es.uah.cc.ie.harvestedsnsangelguard.Main" -Dexec.args="https://localhost:8443/ /Users/josejavierblecuadepedro1/.keystore blecua" >> $logFile
